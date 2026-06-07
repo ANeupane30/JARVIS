f36b9ceec847cd3a9ea5b3440c4de856803d074c
aseemneupane30@gmail.com <aseemneupane30@gmail.com>
2026-06-07 19:24:03 +0930

    added speech-to-text function in listener.py for listening user query

--- modules/voice/listener.py
+++ modules/voice/listener.py
@@ -1,2 +1,25 @@
-import speech_recognition as sr
+import io
+from faster_whisper import WhisperModel
 
+"""
+This functio
+"""
+
+def speech_to_text(audio_data):
+    model_size = "base"
+    model = WhisperModel(model_size, device="cpu", compute_type="default")
+    # Reading the audi bytes using io.BytesIO
+    segments, info = model.transcribe(io.BytesIO(audio_data), beam_size=5)
+    
+    try:
+        # extracting text from segment
+        for segment in segments:
+            text = segment.text
+        
+        return text
+    
+    except Exception as e:
+        return {"status": "error", "message": f"Error during transcription: {str(e)}"}
+        
+
+    
