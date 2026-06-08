301ff9c0824db1e7e8d2fdfecf2fa4461d42f757
aseemneupane30@gmail.com <aseemneupane30@gmail.com>
2026-06-08 21:50:04 +0930

    added background listner function in listener.py

--- modules/voice/listener.py
+++ modules/voice/listener.py
@@ -1,25 +1,45 @@
-import io
-from faster_whisper import WhisperModel
+import sounddevice as sd
+import configparser
+import queue
+import numpy as np
 
-"""
-This functio
-"""
 
-def speech_to_text(audio_data):
-    model_size = "base"
-    model = WhisperModel(model_size, device="cpu", compute_type="default")
-    # Reading the audi bytes using io.BytesIO
-    segments, info = model.transcribe(io.BytesIO(audio_data), beam_size=5)
-    
-    try:
-        # extracting text from segment
-        for segment in segments:
-            text = segment.text
-        
-        return text
-    
-    except Exception as e:
-        return {"status": "error", "message": f"Error during transcription: {str(e)}"}
-        
+# initializing the parser and Queue
+config = configparser.ConfigParser()
+audio_queue = queue.Queue()
 
+# load the file
+config.read("/config/sounddevice.config")
+#loading values
+sample_rate = config.SAMPLE_RATE
+block_duration = config.BLOCK_DURATION
+channels = config.CHANNELS
+
+
+
+def audio_callback(indata, frames, time_info, status):
+    "This function is called for each audio block by sounddevice"
+    if status:
+        print(status)
+    # putting a copy of the audio data into the queue
     
+def process_audio_stream():
+    #start the stream
+    stream = sd.InputStream(
+        samplerate=sample_rate,
+        channels=channels,
+        callback=audio_callback,
+        blocksize=int(sample_rate * block_duration)
+    )
+    
+    print("Listening for keywords...")
+    with stream: 
+        while True:
+            # Get the raw 30ms block of audio
+            audio_block = audio_queue.get()
+            
+            # TO-DO: Convert the audio block to features
+            # add pass to your keyword spotting model
+            
+            
+            pass
---
+++ modules/voice/speech_to_text.py
@@ -0,0 +1,25 @@
+import io
+from faster_whisper import WhisperModel
+
+"""
+This function takes audio data in byte format then converts the speech to text. 
+"""
+
+def speech_to_text(audio_data):
+    model_size = "base"
+    model = WhisperModel(model_size, device="cpu", compute_type="default")
+    # Reading the audi bytes using io.BytesIO
+    segments, info = model.transcribe(io.BytesIO(audio_data), beam_size=5)
+    
+    try:
+        # extracting text from segment
+        for segment in segments:
+            text = segment.text
+        
+        return text
+    
+    except Exception as e:
+        return {"status": "error", "message": f"Error during transcription: {str(e)}"}
+        
+
+    
