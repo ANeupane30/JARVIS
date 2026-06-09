6832bcc8c5b3745f2e57ca499b2a7e7ea1d64a00
aseemneupane30@gmail.com <aseemneupane30@gmail.com>
2026-06-09 15:33:59 +0930

    created process_audio_stream function

--- modules/voice/listener.py
+++ modules/voice/listener.py
@@ -1,20 +1,22 @@
 import sounddevice as sd
 import configparser
 import queue
-import numpy as np
 
+"""
+For now this file contains two function audio_callback() and process_audio_stream(). audio_callback()
+stores short audio clips in queue for processing. Where as process_audio_stream() starts the stream once
+and sounddevice's background thread runs continuously in background and collect the audio clip. 
+"""
 
 # initializing the parser and Queue
 config = configparser.ConfigParser()
 audio_queue = queue.Queue()
+config.read("config/sounddevice.config") # load the file
 
-# load the file
-config.read("/config/sounddevice.config")
 #loading values
-sample_rate = config.SAMPLE_RATE
-block_duration = config.BLOCK_DURATION
-channels = config.CHANNELS
-
+sample_rate = int(config['SAMPLE_RATE'])
+block_duration = float(config['BLOCK_DURATION'])
+channels = int(config['CHANNELS'])
 
 
 def audio_callback(indata, frames, time_info, status):
@@ -25,22 +27,17 @@ def audio_callback(indata, frames, time_info, status):
     audio_queue.put(indata.copy())
     
 def process_audio_stream():
+    """Start background audio stream. Returns the stream object."""
     #start the stream
     stream = sd.InputStream(
         samplerate=sample_rate,
+        dtype='float32',
         channels=channels,
         callback=audio_callback,
         blocksize=int(sample_rate * block_duration)
     )
     
-    print("Listening for keywords...")
-    with stream: 
-        while True:
-            # Get the raw 30ms block of audio
-            audio_block = audio_queue.get()
-            
-            # TO-DO: Convert the audio block to features
-            # add pass to your keyword spotting model
-            
-            
-            pass
+    #starting background listening
+    stream.start()  # this line starts the sound device background thread. 
+    return stream
+    
