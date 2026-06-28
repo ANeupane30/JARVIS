2f8e605b90a98df37ffb2e3dc0f8c1a9d92a9ad2
aseemneupane30@gmail.com <aseemneupane30@gmail.com>
2026-06-28 11:11:24 +0930

    unkown changes

--- jarvis/orchestrator/audio_orchestrator.py
+++ jarvis/orchestrator/audio_orchestrator.py
@@ -2,10 +2,11 @@ from jarvis.component.listener import start_audio_stream, audio_queue
 from jarvis.component.wakeup import kws
 from jarvis.component.transcriber import listen_and_transcribe
 from jarvis.component.speaker import speak, prewarm
+from jarvis.component.response_formatter import complete_json_response
+from jarvis.brain.llm import llm_response
+
 import queue as q
 import configparser
-import jarvis.component.speaker as speaker
-import time
 
 
 
@@ -16,6 +17,8 @@ config.read("config/sounddevice.config")  # config for keyword spotter audio
 sample_rate = int(config['kws_audio']['SAMPLE_RATE'])
 block_duration = float(config['kws_audio']['BLOCK_DURATION'])
 channels = int(config['kws_audio']['CHANNELS'])
+
+llm_call = False
 
 def run():
     # start once — never stopped between kws and sr
@@ -29,6 +32,7 @@ def run():
             keyword = kws()
             print("KWS Detected")
             speak("How can I help you")
+            llm_call = True
             
             # flush queue — discard the keyword audio itself so
             # whisper doesn't transcribe "hey jarvis" as the command
@@ -39,8 +43,15 @@ def run():
                     break
 
             # sr reads from the same audio_queue
-            transcript = listen_and_transcribe(block_duration)
-            print(f"You said: {transcript}")
+            while llm_call:
+                transcript = listen_and_transcribe(block_duration)
+                print(f"You said: {transcript}")
+                data = llm_response(transcript)
+                llm_result = complete_json_response(data)
+                print(llm_result)
+                speak(llm_result)
+                llm_call = False
+            # calling LLM
 
     except KeyboardInterrupt:
         print("Stopping.")
